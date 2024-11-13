efd070834bbf28aaea7de4887f0d4a8b3f750f4e
thinkpad_e540 <froggamestudio7@gmail.com>
2024-11-13 12:42:09 +0300

    getting started on deque

--- my_deque/main.cpp
+++ my_deque/main.cpp
@@ -1,6 +1,24 @@
-#include <iostream> 
+#include <exception>
+#include <iostream>
 
-int main() {
-    std :: cout << "hello world" << std :: endl;
-    return 0;
-}
+template <typename T> class deque {
+public:
+  deque() : _map(nullptr), _sz(0), _cap(0) {}
+
+private:
+  T *resrve_chunk() {
+    T *temp_chunk;
+    try {
+      temp_chunk =
+          reinterpret_cast<T *>(new char[sizeof(T) * _deque_chank_size]);
+    } catch (const std ::exception &ex) {
+      std ::cout << ex.what() << std ::endl;
+    }
+  }
+  T **_map;
+  size_t _sz;
+  size_t _cap;
+  static constexpr size_t _deque_chank_size = 32;
+};
+
+int main() {}
