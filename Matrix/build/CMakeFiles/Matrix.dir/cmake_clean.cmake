9a4a4a0bcaba69e8cfcccdf1470ef22d8fc56714
Egor_wsl_main <froggamestudio7@gmail.com>
2024-11-23 19:58:28 +0300

    fixed link error

---
+++ Matrix/build/CMakeFiles/Matrix.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Matrix.dir/sources/main.cpp.o"
+  "CMakeFiles/Matrix.dir/sources/main.cpp.o.d"
+  "CMakeFiles/Matrix.dir/sources/matrix.cpp.o"
+  "CMakeFiles/Matrix.dir/sources/matrix.cpp.o.d"
+  "Matrix"
+  "Matrix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
